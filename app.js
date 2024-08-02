51df50d7ef8cf82e04b388abcf7d36250658d4c1
AbdulJabbarMalik <jabbarmalik0812@gmail.com>
2024-08-02 17:02:13 +0500

    My Portfolio

--- app.js
+++ app.js
@@ -54,3 +54,20 @@ icon.forEach((icon, index) => {
 });
 
 
+//// scroll animation on services section
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }
+    else{
+      entry.target.classList.remove("show");
+    }
+  })
+})
+
+const hiddenElements = document.querySelectorAll(".hidden")
+hiddenElements.forEach((el) => observer.observe(el));
+
